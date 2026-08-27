64a2bfe43c4ca50a9e7beb168368680ff6a990ea
Gustavo <gustavohenriquevanzelli5@gmail.com>
2026-08-27 08:30:36 -0300

    atulização do codigo

--- livros/views.py
+++ livros/views.py
@@ -1,7 +1,20 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import filters
 from .models import Livro
 from .serializers import LivroSerializer
+from .permissions import IsBibliotecarioOrReadOnly
 
 class LivroListCreateView(ListCreateAPIView):
+    queryset = Livro.objects.all().order_by('-criado_em')
+    serializer_class = LivroSerializer
+    permission_classes = [IsBibliotecarioOrReadOnly]
+    
+    # Configuração de Filtros e Busca
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['titulo', 'autor', 'isbn']
+    ordering_fields = ['titulo', 'ano_publicacao', 'criado_em']
+
+class LivroDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    permission_classes = [IsBibliotecarioOrReadOnly]

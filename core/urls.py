ab621326c22f3d1945dd4571258b0bb21f16990f
Gustavo <gustavohenriquevanzelli5@gmail.com>
2026-08-28 08:12:01 -0300

    auteração pasta core

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,23 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
+
+# Views da nossa aplicação
+from core.views import healthcheck
+from usuarios.views import CustomTokenObtainPairView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 🚦 Infraestrutura
+    path('api/v1/healthcheck/', healthcheck, name='healthcheck'),
+
+    # 🔑 Autenticação JWT Customizada (Semana 16)
+    path('api/v1/auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/logout/', LogoutView.as_view(), name='token_logout'),
+    path('api/v1/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # 📚 Apps de Negócio
+    path('api/v1/livros/', include('livros.urls')),
+    path('api/v1/usuarios/', include('usuarios.urls')),
 ]
